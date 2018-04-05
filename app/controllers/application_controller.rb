6d0ce137234aa9d6813104209382baf30ba8e06b
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-05 15:19:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,15 @@ class ApplicationController < Sinatra::Base
   #  erb :index
   #end
 
+  get '/recipes/new' do
+    erb :new
+  end
+   
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredient], :cook_time => params[:cook_time])
+    redirect to "/recipe/#{@recipe.id}"
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     #binding.pry
