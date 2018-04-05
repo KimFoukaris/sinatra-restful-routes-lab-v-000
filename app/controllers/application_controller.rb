fc51b22b99ed532ae6306ae0e47915ef9e539a2f
Kim Foukaris <kimfoukaris@outlook.com>
2018-04-05 13:50:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < Sinatra::Base
-  
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get '/' do
     erb :index
   end
@@ -13,5 +13,5 @@ class ApplicationController < Sinatra::Base
     @recipes = Recipe.all
     erb :index
   end
-  
+
 end
